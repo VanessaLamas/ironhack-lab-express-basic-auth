e04e32cf7f4881360059109b5c238a6c402330d9
John Doe <johndoe@example.com>
2023-05-26 22:16:04 +0200

    added main and funny cat

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const isLoggedIn = require('../middleware/isLoggedIn');
 
 // homepage render
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/main', isLoggedIn, (req,res,next) => {
+  res.render('main');
+})
 
 module.exports = router;
 
